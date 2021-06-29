707b46d79e62efb93c06bab952e6ad3e48e7c2b5
QWR3 <anton004@protonmail.com>
2021-06-29 18:37:52 +0300

    add reducer without useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useEffect, useReducer} from "react";
 import {getAllCharacters} from "./services/API";
 import Characters from "./components/characters/Characters";
 import AboutCharacter from "./components/aboutCharacter/AboutCharacter";
@@ -7,35 +7,55 @@ import Buttons from "./components/buttons/Buttons";
 import Episode from "./components/episode/Episode";
 import Location from "./components/location/Location"
 
-function App() {
-    let [characters, setCharacters] = useState(null)
-    let [aboutCharacter, setAboutCharacter] = useState(null)
-    let [page, setPage] = useState(1)
-    let [episode, setEpisode] = useState(null);
-    let [location, setLocation] = useState(null);
+let reducer = (state, action) => {
+    switch (action.type) {
+        case "setCharacters":
+            return {...state, characters: action.payload}
+        case "setAboutCharacter":
+            return {...state, aboutCharacter: action.payload}
+        case "setPage":
+            return {...state, page: action.payload}
+        case "setEpisode":
+            return {...state, episode: action.payload}
+        case "setLocation":
+            return {...state, location: action.payload}
+    }
+}
 
-    let getLocationFn = (location)=>{
-        setLocation(location)
+function App() {
+    let [state, dispatch] = useReducer(reducer, {
+        characters: null,
+        aboutCharacter: null,
+        page: 1,
+        episode: null,
+        location: null
+    })
+    let {characters, aboutCharacter, page, episode, location} = state;
+
+
+    let getLocationFn = (location) => {
+        dispatch({type: "setLocation", payload: location})
     }
 
     let getEpisodeFn = (epispde) => {
-        setEpisode(epispde)
+        dispatch({type: "setEpisode", payload: epispde})
     }
 
 
     useEffect(() => {
         getAllCharacters(page).then(value => {
-            setCharacters(value.data.results)
+            // console.log(JSON.stringify(value.data.results))
+            dispatch({type: "setCharacters", payload: value.data.results})
         })
     }, [page])
     let pageNext = () => {
-        setPage(page + 1)
+        dispatch({type: "setPage", payload: page + 1})
     }
     let pagePrevious = () => {
-        setPage(page - 1)
+        dispatch({type:"setPage",payload:page - 1})
     }
     let aboutCharacterFn = (character) => {
-        setAboutCharacter(character)
+        dispatch({type:"setAboutCharacter",payload:character})
     }
 
     return (
@@ -43,11 +63,13 @@ function App() {
             <div>
                 <Buttons page={page} pagePrevious={pagePrevious} pageNext={pageNext}/>
                 {characters &&
-                <Characters items={characters} aboutCharacter={aboutCharacterFn} getEpisodeFn={getEpisodeFn} getLocationFn={getLocationFn}/>}
-                <Buttons page={page} pagePrevious={pagePrevious} pageNext={pageNext} />
+                <Characters items={characters} aboutCharacter={aboutCharacterFn} getEpisodeFn={getEpisodeFn}
+                            getLocationFn={getLocationFn}/>}
+                <Buttons page={page} pagePrevious={pagePrevious} pageNext={pageNext}/>
             </div>
             <div>
-                {aboutCharacter && <AboutCharacter character={aboutCharacter} getEpisodeFn={getEpisodeFn} getLocationFn={getLocationFn}/>}
+                {aboutCharacter &&
+                <AboutCharacter character={aboutCharacter} getEpisodeFn={getEpisodeFn} getLocationFn={getLocationFn}/>}
                 {episode && <Episode episode={episode}/>}
                 {location && <Location location={location}/>}
             </div>
--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,10 +1,13 @@
 import './Characters.css';
 import Character from "../character/Character";
 
-export default function Characters({items,aboutCharacter , getEpisodeFn , getLocationFn}) {
+export default function Characters({items, aboutCharacter, getEpisodeFn, getLocationFn}) {
     return (
         <div className={"characters"}>
-            {items.map((value, index) => <Character key={index} character={value} aboutCharacter={aboutCharacter} getEpisodeFn={getEpisodeFn} getLocationFn={getLocationFn}/>)}
+            {items.map((value, index) => <Character key={index} character={value}
+                                                                aboutCharacter={aboutCharacter}
+                                                                getEpisodeFn={getEpisodeFn}
+                                                                getLocationFn={getLocationFn}/>)}
         </div>
     );
 }
