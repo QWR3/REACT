b2f9b9df87d93e75188ee3931e2cb2c235d22cc5
QWR3 <anton004@protonmail.com>
2021-07-01 22:08:09 +0300

    I add react router dom

--- src/components/aboutCharacter/AboutCharacter.js
+++ src/components/aboutCharacter/AboutCharacter.js
@@ -1,14 +1,15 @@
 import './AboutCharacter.css'
-import {useEffect, useState} from "react";
+import {useEffect, useReducer, useState} from "react";
 import {aboutCharacter, getEpisode, getLocation} from "../../services/API";
 import {Link, Route} from "react-router-dom";
 
-export default function AboutCharacter({id, getEpisodeFn, getLocationFn,url}) {
+export default function AboutCharacter({id, getEpisodeFn, getLocationFn, url}) {
     let [episode, setEpisode] = useState([]);
     let [location, setLocation] = useState([]);
     let [character, setCharacter] = useState([])
     useEffect(() => {
         id && aboutCharacter(id).then(value => setCharacter(value.data))
+
     }, [id])
     let {id: characterId, name, status, species, gender, location: locationUrl, image, episode: episodeUrls} = character
     useEffect(() => {
@@ -31,17 +32,21 @@ export default function AboutCharacter({id, getEpisodeFn, getLocationFn,url}) {
                     <h4>{status} - {species}</h4>
                     <h3 className={'color'}>Last known location:</h3>
                     <h3> {locationUrl.name}</h3>
-                    <Link to={url+'/location'}>
+                    <Link to={url + '/location'}>
                         <button>about location</button>
                     </Link>
                     <h3 className={'color'}>First seen in:</h3>
                     <h3>{episode.name}</h3>
-                    <Link to={url+'/episode'}>
+                    <Link to={url + '/episode'}>
                         <button>about episode</button>
                     </Link>
                 </div>
-                <Route path={url+'/location'} render={()=>{getLocationFn(location)}}/>
-                <Route path={url+'/episode'} render={()=>{getEpisodeFn(episode)}}/>
+                <Route path={url + '/location'} render={() => {
+                    getLocationFn(location)
+                }}/>
+                <Route path={url + '/episode'} render={() => {
+                    getEpisodeFn(episode)
+                }}/>
             </div>
         );
     } else {
